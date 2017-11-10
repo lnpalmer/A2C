4051076db3cf257722337ee20380ab0f88fb56df
Lukas Palmer <lukas@lnpalmer.io>
2017-11-10 14:10:29 -0500

    LSTM -> LSTMCell

--- models.py
+++ models.py
@@ -53,7 +53,7 @@ class AtariNet(nn.Module):
                                   nn.Conv2d(32, 32, 3, stride=2, padding=1),
                                   nn.ELU(inplace=True))
 
-        self.lstm = nn.LSTM(32 * 3 * 3, lstm_size)
+        self.lstm = nn.LSTMCell(32 * 3 * 3, lstm_size)
 
         self.policy = nn.Linear(lstm_size, policy_size)
         self.value = nn.Linear(lstm_size, 1)
@@ -69,35 +69,19 @@ class AtariNet(nn.Module):
         self.value.weight.data = normalized_columns(self.value.weight.size())
 
     def forward(self, conv_in, lstm_state_in):
-        """ PyTorch forward pass
+        """ PyTorch forward pass """
+        conv_out = self.conv(conv_in)
 
-        Args:
-            conv_in (torch.Tensor): input observation, [M x N x 1 x 42 x 42]
-            lstm_state_in ((torch.Tensor, torch.Tensor)): LSTM state tuple,
-                [1 x N x self.lstm_size], [1 x N x self.lstm_size]
+        lstm_in = conv_out.view(-1, 3 * 3 * 32)
+        lstm_state_out = self.lstm(lstm_in, lstm_state_in)
+        lstm_out, _ = lstm_state_out
 
-        Returns:
-            policy (torch.Tensor): policy, [M x N x self.policy_size]
-            value (torch.Tensor): value estimate, [M x N x 1]
-            lstm_state_out ((torch.Tensor, torch.Tensor)): new LSTM state,
-                [1 x N x self.lstm_size], [1 x N x self.lstm_size]
-        """
-        seq_len, batch_size, _, _, _ = conv_in.size()
+        policy = self.policy(lstm_out)
+        value = self.value(lstm_out)
 
-        conv_out = self.conv(conv_in.view(-1, 1, 42, 42))
+        return policy, value, lstm_state_out
 
-        lstm_in = conv_out.view(seq_len, batch_size, 32 * 3 * 3)
-        lstm_h, lstm_c = lstm_state_in
-        lstm_out, lstm_state_out = self.lstm(lstm_in, lstm_state_in)
-
-        policy = self.policy(lstm_out.view(-1, self.lstm_size))
-        value = self.value(lstm_out.view(-1, self.lstm_size))
-
-        return policy.view(seq_len, batch_size, -1), \
-               value.view(seq_len, batch_size, -1), \
-               lstm_state_out
-
-    def make_state(self):
+    def make_state(self, count=1):
         """ Makes an initial network state """
-        return (Variable(torch.zeros(1, 1, self.lstm_size)),
-                Variable(torch.zeros(1, 1, self.lstm_size)))
+        return (Variable(torch.zeros(count, self.lstm_size)),
+                Variable(torch.zeros(count, self.lstm_size)))
