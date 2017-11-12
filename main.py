f747cde09e9bec2d538ee5b06de27154b4dbe9ca
Lukas Palmer <lukas@lnpalmer.io>
2017-11-12 01:39:41 -0500

    name revert

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ from train import train
 parser = argparse.ArgumentParser(description='A2C (Advantage Actor-Critic)')
 parser.add_argument('env_name', type=str, help='Gym environment id')
 parser.add_argument('--no-cuda', action='store_true', help='use to disable available CUDA')
-parser.add_argument('--num-envs', type=int, default=4, help='number of parallel environments')
+parser.add_argument('--num-workers', type=int, default=4, help='number of parallel workers')
 parser.add_argument('--rollout-steps', type=int, default=20, help='steps per rollout')
 parser.add_argument('--total-steps', type=int, default=int(4e7), help='total number of steps to train for')
 parser.add_argument('--lr', type=float, default=3e-4, help='learning rate')
